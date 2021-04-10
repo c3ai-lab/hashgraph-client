bb7b2a8d2ea65b62efeb187c9ae1d979ff9f9c31
Yannik Goldgräbe <ykg@live.de>
2021-04-10 10:41:50 +0200

    Added arguments to client usage and transaction status functionality

--- client.py
+++ client.py
@@ -7,41 +7,77 @@ from thrift.transport import TSSLSocket
 from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
 
-import time
-import random
+import time, uuid, random, sys, getopt, argparse
 from random import randrange
 
 
+#python3 client.py -n localhost -p 9090 --calls 1000 --delay 1
+#python3 client.py -n localhost -p 9090 --amount 99 --target 1
+#python3 client.py -n localhost -p 9090 --status e45cc861a742436f9a27f88c081433d1
 def main():
-    # Make socket
-    transport = TSSLSocket.TSSLSocket(host='localhost', port=9090, certfile='.ssh/client.crt', keyfile='.ssh/client.key', ca_certs='.ssh/CA.pem')
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--node", "-n", help="Define node address")
+    parser.add_argument("--port", "-p", help="Define node port")
+    parser.add_argument("--calls", "-c", help="Set amount of transaction calls to be done against the node")
+    parser.add_argument("--delay", "-d", help="Set delay between amount of transaction calls")
+    parser.add_argument("--amount", "-a", help="Set amount to be transferred")
+    parser.add_argument("--target", "-t", help="Set target for transfer")
+    parser.add_argument("--status", "-s", help="Request status from transaction id")
 
-    # Buffering is critical. Raw sockets are very slow
+    try:
+        args = parser.parse_args()
+    except:
+        parser.print_help()
+        sys.exit(0)
+
+    if args.node and args.port:
+        print("Set node to %s" % args.node)
+        print("Set port to %s" % args.port)
+        transport = TSSLSocket.TSSLSocket(host=args.node, port=args.port, certfile='.ssh/client.crt', keyfile='.ssh/client.key', ca_certs='.ssh/CA.pem')
+    elif args.node:
+        print("Set node to %s" % args.node)
+        transport = TSSLSocket.TSSLSocket(host=args.node, certfile='.ssh/client.crt', keyfile='.ssh/client.key', ca_certs='.ssh/CA.pem')
+    else:
+        parser.print_help()
+        sys.exit(0)
+
     transport = TTransport.TBufferedTransport(transport)
-
-    # Wrap in a protocol
     protocol = TBinaryProtocol.TBinaryProtocol(transport)
-
-    # Create a client to use the protocol encoder
     client = Transaction.Client(protocol)
 
-    # Load test parameters
+    if args.status:
+        transport.open()
+        status(args.status, client)
+        transport.close()
+    elif args.amount and args.target:
+        transport.open()
+        transfer(int(args.amount), int(args.target), client)
+        transport.close()
+    elif args.calls and args.delay:
+        transport.open()
+        benchmark(int(args.calls), int(args.delay), client)
+        transport.close()
+    else:
+        parser.print_help()
+        sys.exit(0)
+
+def benchmark(calls, delay, client):
     starttime = time.time()
-    amount_calls = 1000
-    delay_seconds = 1.0
-
-    # Connect!
-    transport.open()
-
     while True:
-        print("Executing " + str(amount_calls) + " calls...")
-        for _ in range(amount_calls):
-            client.transfer(randrange(100), random.randint(-1,3))
-        time.sleep(delay_seconds - ((time.time() - starttime) % delay_seconds))
+        print("Executing " + str(calls) + " transactions...")
+        for _ in range(calls):
+            transfer(randrange(100), random.randint(-1,3), client)
+        time.sleep(delay - ((time.time() - starttime) % delay))
 
-    # Close!
-    transport.close()
+def transfer(amount, target, client):
+    tx_id = client.transfer(amount, target)
+    print('tx_id: %s' % tx_id)
 
+def status(tx_id, client):
+    status = client.status(tx_id)
+    print('status: %s' % status.status)
+    if status.consensus_time != 0:
+        print('consensus time: %s' % status.consensus_time)
 
 if __name__ == '__main__':
     try:
